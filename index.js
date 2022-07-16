590ca82cb3d8de6f2a0401ff99ee578b23cd8554
Elwin <thinkerelwin@gmail.com>
2022-07-17 06:38:39 +0800

    parsing the csv data

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+import { parse } from "csv-parse";
 import fs from "fs";
-let results = [];
+let habitablePlanet = [];
+function isHabitablePlanet(disposition) {
+    return disposition === "CONFIRMED";
+}
+function idealInsolationFlux(earthFlux) {
+    return earthFlux > 0.36 && earthFlux <= 1.11;
+}
+function isPossibleToHaveSolidSurface(radius) {
+    return radius <= 1.6;
+}
 fs.createReadStream("./kepler_data_20220709.csv")
+    .pipe(parse({
+    comment: "#",
+    columns: true,
+}))
     .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data["koi_disposition"]) &&
+        idealInsolationFlux(data["koi_insol"]) &&
+        isPossibleToHaveSolidSurface(data["koi_prad"])) {
+        habitablePlanet.push(data);
+    }
 })
     .on("error", (err) => {
     console.log(err);
 })
     .on("end", () => {
-    console.log("result: ", results);
+    console.log(habitablePlanet.map((planet) => planet["kepler_name"]));
+    console.log(`${habitablePlanet.length} habitable planets found `);
 });
--- index.ts
+++ index.ts
@@ -1,15 +1,47 @@
 import { parse } from "csv-parse";
 import fs from "fs";
 
-let results: string[] = [];
+interface Planet {
+  koi_disposition: string;
+  koi_insol: number;
+  koi_prad: number;
+  kepler_name: string;
+}
+
+let habitablePlanet: Planet[] = [];
+
+function isHabitablePlanet(disposition: string) {
+  return disposition === "CONFIRMED";
+}
+
+function idealInsolationFlux(earthFlux: number) {
+  return earthFlux > 0.36 && earthFlux <= 1.11;
+}
+
+function isPossibleToHaveSolidSurface(radius: number) {
+  return radius <= 1.6;
+}
 
 fs.createReadStream("./kepler_data_20220709.csv")
-  .on("data", (data: string) => {
-    results.push(data);
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data: Planet) => {
+    if (
+      isHabitablePlanet(data["koi_disposition"]) &&
+      idealInsolationFlux(data["koi_insol"]) &&
+      isPossibleToHaveSolidSurface(data["koi_prad"])
+    ) {
+      habitablePlanet.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log("result: ", results);
+    console.log(habitablePlanet.map((planet) => planet["kepler_name"]));
+    console.log(`${habitablePlanet.length} habitable planets found `);
   });
