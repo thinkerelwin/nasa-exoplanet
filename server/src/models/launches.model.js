db90b47250bdc3b39f49e0a35ab3d3f0bd1b3b9f
Elwin <thinkerelwin@gmail.com>
2022-07-26 21:37:08 +0800

    add newLaunch method

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,16 +1,22 @@
 const launches = new Map();
+let latestFlightNumber = 100;
+const defaultCustomers = ["Elwin", "NASA"];
 const launch = {
-    flightNumber: 100,
+    flightNumber: latestFlightNumber,
     mission: "Kepler Exploration X",
     rocket: "Explorer IS1",
     launchDate: new Date("December 27, 2030"),
     destination: "Kepler-442 b",
-    customer: ["Elwin", "NASA"],
+    customer: defaultCustomers,
     upcoming: true,
     success: true,
 };
-launches.set(launch.flightNumber, launch);
+launches.set(latestFlightNumber, launch);
 function getAllLaunches() {
     return [...launches.values()];
 }
-export { getAllLaunches };
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(latestFlightNumber, Object.assign(Object.assign({}, launch), { flightNumber: latestFlightNumber }));
+}
+export { getAllLaunches, addNewLaunch };
--- server/src/models/launches.model.ts
+++ server/src/models/launches.model.ts
@@ -1,20 +1,42 @@
 const launches = new Map();
 
-const launch = {
-  flightNumber: 100,
+let latestFlightNumber = 100;
+const defaultCustomers = ["Elwin", "NASA"];
+
+interface Launch {
+  flightNumber: number;
+  mission: string;
+  rocket: string;
+  launchDate: Date;
+  destination: string;
+  customer: string[];
+  upcoming: boolean;
+  success: boolean;
+}
+
+const launch: Launch = {
+  flightNumber: latestFlightNumber,
   mission: "Kepler Exploration X",
   rocket: "Explorer IS1",
   launchDate: new Date("December 27, 2030"),
   destination: "Kepler-442 b",
-  customer: ["Elwin", "NASA"],
+  customer: defaultCustomers,
   upcoming: true,
   success: true,
 };
 
-launches.set(launch.flightNumber, launch);
+launches.set(latestFlightNumber, launch);
 
 function getAllLaunches() {
   return [...launches.values()];
 }
 
-export { getAllLaunches };
+function addNewLaunch(launch: Launch) {
+  latestFlightNumber++;
+  launches.set(latestFlightNumber, {
+    ...launch,
+    flightNumber: latestFlightNumber,
+  });
+}
+
+export { getAllLaunches, addNewLaunch };
